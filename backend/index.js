46c8613f7fdd09d8165798604beccec74875eea2
Piyush <piyushpatil0031@gmail.com>
2025-12-20 10:56:37 +0530

    cors updated for deployement

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "http://localhost:3000", // your frontend
+  // origin: "http://localhost:3000", // your frontend
+  origin: "unisphere-delta.vercel.app", // your frontend
   credentials: true
 }));
 
