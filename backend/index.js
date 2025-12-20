92916335115e3008be6f5020b81869e33babd6bb
Piyush <piyushpatil0031@gmail.com>
2025-12-20 11:02:27 +0530

    cors updated for deployement

--- backend/index.js
+++ backend/index.js
@@ -37,10 +37,16 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  // origin: "http://localhost:3000", // your frontend
-  origin: "unisphere-delta.vercel.app", // your frontend
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true); // allow server-to-server
+    if (origin.endsWith(".vercel.app")) {
+      return callback(null, true);
+    }
+    callback(new Error("Not allowed by CORS"));
+  },
   credentials: true
 }));
+
 
 // Routes
 app.use("/api/clubs", clubRoutes);
